ea8e0b30593b7fd5720d786f3ca312568f0f88e7
zaheer-bashir <zaheerkpr3@gmail.com>
2024-11-29 02:49:41 +0500

    Updates image domain configuration for asset loading

--- next.config.mjs
+++ next.config.mjs
@@ -7,29 +7,7 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    domains: ["localhost"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.sanity.io",
-        port: "",
-      },
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-        port: "",
-      },
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-        port: "",
-      },
-      {
-        protocol: "https",
-        hostname: "pub-b7fd9c30cdbf439183b75041f5f71b92.r2.dev",
-        port: "",
-      },
-    ],
+    domains: ["velour-web.s3.amazonaws.com"],
   },
   webpack(config) {
     config.module.rules.push({
