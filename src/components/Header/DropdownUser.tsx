c1cd361a47666ddf473454295dc7c26d0a4559bb
zaheer-bashir <zaheerkpr3@gmail.com>
2024-12-05 00:14:41 +0500

    Logout User on Logout

--- src/components/Header/DropdownUser.tsx
+++ src/components/Header/DropdownUser.tsx
@@ -80,6 +80,7 @@ const DropdownUser = () => {
             </span>
           </div>
           <div className="p-2.5">
+            <Link href="/login">
             <button className="flex w-full items-center gap-2.5 rounded-[7px] p-2.5 text-sm font-medium text-dark-4 duration-300 ease-in-out hover:bg-gray-2 hover:text-dark dark:text-dark-6 dark:hover:bg-dark-3 dark:hover:text-white lg:text-base">
               <svg
                 className="fill-current"
@@ -107,6 +108,7 @@ const DropdownUser = () => {
               </svg>
               Logout
             </button>
+            </Link>
           </div>
         </div>
       )}
